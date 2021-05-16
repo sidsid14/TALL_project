43710b109482b6e08b978fab2e3b6c0798103f11
sudhanshu tiwari <sudhanshu@viosmedical.com>
2021-05-16 19:43:17 +0530

    Custom reusable modal component

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,13 +1,15 @@
- <div class="flex flex-col bg-indigo-900 w-full h-screen" x-data="{showSubscribe: false,}">
+ <div class="flex flex-col bg-indigo-900 w-full h-screen"
+      x-data="{showSubscribe: false, showSuccess: true}">
      <nav class="flex pt-5 justify-between container mx-auto text-indigo-200">
-         <a class="text-4xl font-bold 2xl:ml-4" href="/">
+         <a class="text-4xl font-bold 2xl:ml-4"
+            href="/">
              <x-application-logo class="w-16 h-16 fill-current"></x-application-logo>
          </a>
          <div class="flex justify-end 2xl:mr-4">
              @auth
-             <a href="{{ route('dashboard') }}">Dashboard</a>
+                 <a href="{{ route('dashboard') }}">Dashboard</a>
              @else
-             <a href="{{ route('login') }}">Login</a>
+                 <a href="{{ route('login') }}">Login</a>
              @endauth
          </div>
      </nav>
@@ -16,28 +18,39 @@
              <h1 class="text-white font-bold text-5xl leading-tight mb-4">Simple generic landing page to
                  subscribe</h1>
              <p class="text-indigo-200 text-xl mb-10">We are just checking the <span
-                     class="font-bold underline">TALL</span> stack. Would you mind subscribing?</p>
-             <x-button class="py-3 px-8 bg-red-500 hover:bg-red-600" x-on:click="showSubscribe = true">Subscribe
+                       class="font-bold underline">TALL</span> stack. Would you mind subscribing?</p>
+             <x-button class="py-3 px-8 bg-red-500 hover:bg-red-600"
+                       x-on:click="showSubscribe = true">Subscribe
              </x-button>
          </div>
      </div>
-     <div class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full" x-show="showSubscribe"
-         x-on:click.self="showSubscribe = false" x-on:keydown.escape.window="showSubscribe = false">
-         <div class="m-auto bg-pink-500 shadow-2xl rounded-xl p-8">
-             <p class="text-white text-5xl font-extrabold text-center">Let's do it!</p>
-             <form 
-                class="flex flex-col items-center p-24"
-                wire:submit.prevent="subscribe">
-                 <x-input 
-                    class="px-5 py-3 w-80 border border-blue-400"
-                    type="email" 
-                    name="email"
-                    placeholder="Email address"
-                    wire:model="email"></x-input>
-                 <span class="text-gray-100 text-xs">We will send you a confirmation email.</span>
-                 <x-button class="px-5 py-8 mt-5 w-80 bg-blue-500 justify-center">Get In</x-button>
-             </form>
-         </div>
-     </div>
+
+     <x-modal class="bg-pink-500"
+              trigger="showSubscribe">
+         <p class="text-white text-5xl font-extrabold text-center">Let's do it!</p>
+         <form class="flex flex-col items-center p-24"
+               wire:submit.prevent="subscribe">
+             <x-input class="px-5 py-3 w-80 border border-blue-400"
+                      type="email"
+                      name="email"
+                      placeholder="Email address"
+                      wire:model="email"></x-input>
+             <span class="text-gray-100 text-xs">We will send you a confirmation email.</span>
+             <x-button class="px-5 py-8 mt-5 w-80 bg-blue-500 justify-center">Get In</x-button>
+         </form>
+     </x-modal>
+
+     <x-modal class="bg-green-500"
+              trigger="showSuccess">
+         <p class="animate-pulse text-white text-9xl font-extrabold text-center">
+             &check;
+         </p>
+         <p class="text-white text-5xl font-extrabold text-center mt-16">
+             Great!
+         </p>
+         <p class="text-white text-3xl text-center">
+             See you in your inbox
+         </p>
+     </x-modal>
 
  </div>
