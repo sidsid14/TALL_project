0bd4c014de64e67c503c32ffa55b9781d90e4ba3
sudhanshu tiwari <sudhanshu@viosmedical.com>
2021-05-18 17:22:16 +0530

    Livewire dashboard component with delete and search

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -15,4 +15,9 @@ class SubscriberController extends Controller
 
         return redirect('/?verified=1');
     }
+
+    public function all()
+    {
+        return view('subscribers.all');
+    }
 }
